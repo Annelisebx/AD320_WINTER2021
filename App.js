d705b6f78862ceebb115543f140ef6c244033d85
Annelisebx <50610153+Annelisebx@users.noreply.github.com>
2021-01-27 00:48:12 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import MyHeader from './components/MyHeader'
+import './App.css'
+
+const myApp = () => {
+  return (
+    <div className= 'intro'>
+      <MyHeader></MyHeader>
+    </div>
+  );
+}
+export default myApp;
+
